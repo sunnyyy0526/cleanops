43d2853c2086904c67790f3eccd7c9f50a302f5d
Sandeep Kumar <sandeepsunny0078@gmail.com>
2025-12-31 19:03:59 +0530

    update my app.js

--- cleanops/src/services/api.js
+++ cleanops/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true, // 🔥 REQUIRED
+  baseURL: "https://cleanops-lwt1.onrender.com/api",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
