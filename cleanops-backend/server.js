1fd616f4ad76e5ef0b74c7b4472cd445f1544a23
Sandeep Kumar <sandeepsunny0078@gmail.com>
2025-12-31 18:41:03 +0530

    Add .gitignore and remove node_modules

--- cleanops-backend/server.js
+++ cleanops-backend/server.js
@@ -19,12 +19,14 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://cleanops-lwt1.onrender.com" // backend
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.options("*", cors());
 
@@ -50,12 +52,17 @@ app.use("/api/community", communityRoutes);
 /* ======================
    SERVER
 ====================== */
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () =>
-      console.log("Server running on http://localhost:5000")
-    );
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
-  .catch((err) => console.error(err));
+  .catch(err => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
